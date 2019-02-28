7752295e76f0510ed01ce8eecada71b04782f4c8
DivierRomero <46934336+DivierRomero@users.noreply.github.com>
2019-02-27 19:20:00 -0500

    ´ñlññ
    ñññ

---
+++ sim.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib as plt
+masa=147 #Masa del kart [kg]
+c_f=0.825 # coeficiente de fricción
+A_front=0.575 #[m/s^2]
+CG_L=1.074 #Centro de gravedad Longitudinal[m]
+CG_V=0.5 #Centro de gravedad  Vertical [m]
+gravedad=9.78 #Gravedad [m/s^2]
+rho_a=0.95#Densidad del aire
